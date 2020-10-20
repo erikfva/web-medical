a4be81366c2896e2f25e36e0fc18605a6fa151b2
Erik F. Vargas Agreda <var.erick@gmail.com>
2020-10-20 17:53:51 -0400

    First commit.

---
+++ web-medical/contact_process.php
@@ -0,0 +1,74 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+
+require 'vendor/autoload.php';
+
+//Create a new PHPMailer instance
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->Mailer = "smtp";
+
+//Configuracion servidor mail
+//$mail->From = "sistemamedical.info@gmail.com"; //remitente
+$mail->SMTPDebug = 1;
+
+$mail->SMTPAuth = true;
+$mail->SMTPSecure = 'tls'; //seguridad
+$mail->Host = "smtp.gmail.com"; // servidor smtp
+$mail->Port = 587; //puerto
+$mail->Username = 'sistemamedical.info@gmail.com'; //nombre usuario
+$mail->Password = '4rm4g3d0n'; //contraseña
+
+$to = "sistemamedical.info@gmail.com";
+$from = $_REQUEST['email'];
+$name = $_REQUEST['name'];
+$csubject = $_REQUEST['subject'];
+//$number = $_REQUEST['number'];
+$cmessage = $_REQUEST['message'];
+
+$headers = "From: $from";
+$headers = "From: " . $from . "\r\n";
+$headers .= "Reply-To: " . $from . "\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+$subject = "Medic@l mensaje de contacto.";
+
+$logo = 'img/logo.png';
+$link = '#';
+
+$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
+$body .= "<table style='width: 100%;'>";
+$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
+$body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
+$body .= "</td></tr></thead><tbody><tr>";
+$body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
+$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
+$body .= "</tr>";
+$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$csubject}</td></tr>";
+$body .= "<tr><td></td></tr>";
+$body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
+$body .= "</tbody></table>";
+$body .= "</body></html>";
+
+//$send = mail($to, $subject, $body, $headers);
+$mail->IsHTML(true);
+
+//Agregar destinatario
+$mail->AddAddress($to);
+$mail->SetFrom($from, "Medic@l");
+
+$mail->Subject = $subject;
+$mail->Body = $body;
+
+//Avisar si fue enviado o no y dirigir al index
+$output = $mail->Send() ? '{"success":1, "msg": "Gracias por comunicarte con nosotros. Te responderemos lo mas pronto posible."}' : '{"success":0, "msg": "No se ha podido enviar tu mensaje, por favor intentalo nuevamente."}';
+
+// Clean output buffer
+if (ob_get_length()) {
+    ob_end_clean();
+}
+header('Access-Control-Allow-Origin: *'); //Permitir cross-domain
+//header("Content-Type: application/json");
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+echo $output;
